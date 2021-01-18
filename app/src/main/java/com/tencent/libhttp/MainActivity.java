73f71a4765fc24add85a73385f7a6b44585b4d80
v_sdecen <v_sdecen@tencent.com>
2021-01-18 09:58:05 +0800

    --all

--- app/src/main/java/com/tencent/libhttp/MainActivity.java
+++ app/src/main/java/com/tencent/libhttp/MainActivity.java
@@ -1,15 +1,13 @@
 package com.tencent.libhttp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import com.tencent.libhttp.http.HttpCallback;
 import com.tencent.libhttp.http.HttpMessage;
 import com.tencent.libhttp.http.HttpService;
@@ -24,13 +22,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void request(View view) {
-        String url="https://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&hs=0&pn=1&spn=0&di=6160&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=0&ie=utf-8&oe=utf-8&in=&cl=2&lm=-1&st=undefined&cs=3363295869%2C2467511306&os=892371676%2C71334739&simid=4203536407%2C592943110&adpicid=0&lpn=0&ln=1469&fr=&fmq=1610705379420_R&fm=&ic=undefined&s=undefined&hd=undefined&latest=undefined&copyright=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&ist=&jit=&cg=&bdtype=0&oriquery=&objurl=https%3A%2F%2Fgimg2.baidu.com%2Fimage_search%2Fsrc%3Dhttp%3A%2F%2Fa0.att.hudong.com%2F30%2F29%2F01300000201438121627296084016.jpg%26refer%3Dhttp%3A%2F%2Fa0.att.hudong.com%26app%3D2002%26size%3Df9999%2C10000%26q%3Da80%26n%3D0%26g%3D0n%26fmt%3Djpeg%3Fsec%3D1613297382%26t%3D18aa6ca1a376459c9e2fed54123c8678&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3Bfhyvg8_z%26e3Bv54AzdH3F4AzdH3Fetjo_z%26e3Brir%3Fwt1%3Dmb9l9&gsm=2&rpstart=0&rpnum=0&islist=&querylist=&force=undefined";
+        String url = "";
         HttpService.get(url)
+                .addParam("param1", "value1")
+                .addParam("param2", "value2")
+                .addParam("param3", "value3")
                 .doAsync(this, new HttpCallback() {
             @Override
             public void onSucceed(@NonNull HttpMessage msg) {
-                Toast.makeText(MainActivity.this,"succeed"+msg,Toast.LENGTH_SHORT).show();
-                mTvContent.setText(msg.toString());
+                UserBean user = msg.parseObject(UserBean.class);//解析对象。
+//                user=msg.parseObject(UserBean.class,"key");//根据key来解析特定json字段。
+//                List<UserBean> users=msg.parseArray(UserBean.class);//解析数组。
+//                users=msg.parseArray(UserBean.class,4);//根据index解析特定json字段。
+
+                if (user != null) {
+                    mTvContent.setText(user.toString());
+                }
+
+//                Toast.makeText(MainActivity.this,"succeed"+msg,Toast.LENGTH_SHORT).show();
             }
 
             @Override
---
+++ app/src/main/java/com/tencent/libhttp/UserBean.java
@@ -0,0 +1,10 @@
+package com.tencent.libhttp;
+
+/**
+ * Author：岑胜德 on 2021/1/15 19:21
+ *
+ * 说明：
+ */
+public class UserBean {
+
+}
